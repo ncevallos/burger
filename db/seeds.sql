53ff0a4ed6ce08fb42f7c3a723a0114e26b5782d
ncevallos <nmcevall@gmail.com>
2017-09-27 09:32:44 -0400

    Basic App organized and created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, time) VALUES ('Classic Single', CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, time) VALUES ('Double Cheeseburger', CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, time) VALUES ('Double Bacon Cheeseburger', CURRENT_TIMESTAMP);
